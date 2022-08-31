b8747e4c129c465a4c189721f4c5562f873ba18d
theFahim <abdul15-980@diu.edu.bd>
2022-08-31 22:05:24 +0600

    minor changes on bookings decline messege

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -7,23 +7,22 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $bookings = Booking::latest()->filter(request(['search']))->paginate(10);
 
-        return view('admin.booking',compact('bookings'));
+        return view('admin.booking', compact('bookings'));
     }
 
-    public function decline(Booking $booking){
-
+    public function decline(Booking $booking)
+    {
         $booking->delete();
-        return redirect()->route('admin.booking')->withMessage('Booking Request Declined. A Cancellation Request Sent to The Customer');
-
+        return redirect()->route('admin.booking')->withMessage('Booking Request Declined. A Cancellation Email Sent to The Customer');
     }
 
-    public function accept(Booking $booking){
-
+    public function accept(Booking $booking)
+    {
         $booking->delete();
         return redirect()->route('admin.booking')->withMessage('Booking Request Accepted. A Confirmation Email Sent to The Customer');
-
     }
 }
